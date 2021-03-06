249947e5cc07eb56a20b10f141df66dad1f671ee
okothpius <orukopius@gmail.com>
2021-03-06 20:08:30 +0300

    feat: register auth blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -21,8 +21,11 @@ def create_app(config_name):
     moment.init_app(app)
     db.init_app(app)
 
-    from . main import main as main_blueprint
+    from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     # routes and error pages
 
